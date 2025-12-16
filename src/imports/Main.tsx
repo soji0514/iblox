c55bec4744469c5529382a7dad83f46a047ebb7b
soji0514 <158295269+soji0514@users.noreply.github.com>
2025-12-16 18:12:26 +0900

    updated

--- src/imports/Main.tsx
+++ src/imports/Main.tsx
@@ -1,23 +1,3 @@
-import img031 from "figma:asset/fc512dd7c0a271090c4e716135374a28c457980e.png";
-import img01 from "figma:asset/a8d0bf53be3c9b8927316f2d51f1ed699e1c795c.png";
-import img05 from "figma:asset/f741bd2f9caa5e4afefd123a3f3af56cef6e6bdf.png";
-import img02 from "figma:asset/9ded0bbf47eebdc1c006c0eb73cb3aee20df7a4f.png";
-import img07 from "figma:asset/31ec893572695a172521336f173baf609697ed09.png";
-import img04 from "figma:asset/c8069414fa87bd1fa4ea9101c433bccd7fd5b008.png";
-import img06 from "figma:asset/90e2ab50faa5e707b87eed46ef10715a926f3d7b.png";
-import img10 from "figma:asset/e4972b7b3de0df989290e80d24b9ed1eb34e8afb.png";
-import img08 from "figma:asset/df95fd935492ec2ec86ec2967e2cb43b197d8651.png";
-import imgNegativeFreak08 from "figma:asset/94101fd0d97045cd55281becef13ee5399572b55.png";
-import img11 from "figma:asset/50c041ae2c6c807f54033574da043e14bdaeb2b4.png";
-import img14 from "figma:asset/b2a97ced9e243666fc6e55e9411f283b984e39fd.png";
-import img09 from "figma:asset/5d53f24cd755582d0c9ad871d1e8769ad770968d.png";
-import img16 from "figma:asset/ab95ee436339ab26cbdfc902ec87682ec25be8a1.png";
-import img12 from "figma:asset/497f7a2511da3f4f95285e6651a163a4f50223f5.png";
-import imgNegativeFreak09 from "figma:asset/b8312906184bd6b9608fdf28ad4488c5fbe2eb85.png";
-import img13 from "figma:asset/f61e5d1c5f7a27d54222a845b2208293d71a8cf9.png";
-import img15 from "figma:asset/92fb95cc180f558ce566732a2abe0d72c7d56c33.png";
-import imgNegativeFreak11 from "figma:asset/50a4324458595f278a807afc8c68d0d038395989.png";
-
 function Frame() {
   return (
     <div className="content-stretch flex font-['Pretendard:Bold',sans-serif] items-center justify-center leading-[0] not-italic relative shrink-0 text-[#949494] text-[13px] uppercase w-full">
@@ -173,70 +153,6 @@ function Frame13() {
   );
 }
 
-function Frame21() {
-  return (
-    <div className="absolute content-stretch flex flex-col items-start left-[calc(28.57%+14.43px)] top-[-6466px] w-[1066px]">
-      <div className="aspect-[2880/1350] relative shrink-0 w-full" data-name="03 1">
-        <img alt="" className="absolute inset-0 max-w-none object-50%-50% object-cover pointer-events-none size-full" src={img031} />
-      </div>
-      <div className="aspect-[4096/2052] relative shrink-0 w-full" data-name="01">
-        <img alt="" className="absolute inset-0 max-w-none object-50%-50% object-cover pointer-events-none size-full" src={img01} />
-      </div>
-      <div className="aspect-[4096/2304] relative shrink-0 w-full" data-name="05">
-        <img alt="" className="absolute inset-0 max-w-none object-50%-50% object-cover pointer-events-none size-full" src={img05} />
-      </div>
-      <div className="aspect-[4096/1156] relative shrink-0 w-full" data-name="02">
-        <img alt="" className="absolute inset-0 max-w-none object-50%-50% object-cover pointer-events-none size-full" src={img02} />
-      </div>
-      <div className="aspect-[1920/767] relative shrink-0 w-full" data-name="07">
-        <img alt="" className="absolute inset-0 max-w-none object-50%-50% object-cover pointer-events-none size-full" src={img07} />
-      </div>
-      <div className="aspect-[4096/2133] relative shrink-0 w-full" data-name="04">
-        <img alt="" className="absolute inset-0 max-w-none object-50%-50% object-cover pointer-events-none size-full" src={img04} />
-      </div>
-      <div className="aspect-[4096/1634] relative shrink-0 w-full" data-name="06">
-        <img alt="" className="absolute inset-0 max-w-none object-50%-50% object-cover pointer-events-none size-full" src={img06} />
-      </div>
-      <div className="aspect-[4096/2880] relative shrink-0 w-full" data-name="10">
-        <img alt="" className="absolute inset-0 max-w-none object-50%-50% object-cover pointer-events-none size-full" src={img10} />
-      </div>
-      <div className="aspect-[4096/2880] relative shrink-0 w-full" data-name="08">
-        <img alt="" className="absolute inset-0 max-w-none object-50%-50% object-cover pointer-events-none size-full" src={img08} />
-      </div>
-      <div className="aspect-[3840/2160] relative shrink-0 w-full" data-name="Negative Freak08">
-        <img alt="" className="absolute inset-0 max-w-none object-50%-50% object-cover pointer-events-none size-full" src={imgNegativeFreak08} />
-      </div>
-      <div className="aspect-[4096/2660] relative shrink-0 w-full" data-name="11">
-        <img alt="" className="absolute inset-0 max-w-none object-50%-50% object-cover pointer-events-none size-full" src={img11} />
-      </div>
-      <div className="aspect-[4096/2185] relative shrink-0 w-full" data-name="14">
-        <img alt="" className="absolute inset-0 max-w-none object-50%-50% object-cover pointer-events-none size-full" src={img14} />
-      </div>
-      <div className="aspect-[4096/2880] relative shrink-0 w-full" data-name="09">
-        <img alt="" className="absolute inset-0 max-w-none object-50%-50% object-cover pointer-events-none size-full" src={img09} />
-      </div>
-      <div className="aspect-[1920/2236] relative shrink-0 w-full" data-name="16">
-        <img alt="" className="absolute inset-0 max-w-none object-50%-50% object-cover pointer-events-none size-full" src={img16} />
-      </div>
-      <div className="aspect-[1920/1532] relative shrink-0 w-full" data-name="12">
-        <img alt="" className="absolute inset-0 max-w-none object-50%-50% object-cover pointer-events-none size-full" src={img12} />
-      </div>
-      <div className="aspect-[1920/1080] relative shrink-0 w-full" data-name="Negative Freak09">
-        <img alt="" className="absolute inset-0 max-w-none object-50%-50% object-cover pointer-events-none size-full" src={imgNegativeFreak09} />
-      </div>
-      <div className="aspect-[3033/4096] relative shrink-0 w-full" data-name="13">
-        <img alt="" className="absolute inset-0 max-w-none object-50%-50% object-cover pointer-events-none size-full" src={img13} />
-      </div>
-      <div className="aspect-[4096/3183] relative shrink-0 w-full" data-name="15">
-        <img alt="" className="absolute inset-0 max-w-none object-50%-50% object-cover pointer-events-none size-full" src={img15} />
-      </div>
-      <div className="aspect-[1920/1080] relative shrink-0 w-full" data-name="Negative Freak11">
-        <img alt="" className="absolute inset-0 max-w-none object-50%-50% object-cover pointer-events-none size-full" src={imgNegativeFreak11} />
-      </div>
-    </div>
-  );
-}
-
 function Frame14() {
   return (
     <div className="content-stretch flex font-['Pretendard:Bold',sans-serif] items-center leading-[0] not-italic relative shrink-0 text-[8px] text-[rgba(148,148,148,0.6)] uppercase w-full">
@@ -369,14 +285,7 @@ export default function Main() {
     <div className="bg-black relative size-full" data-name="main">
       <div className="absolute bg-white h-[1080px] left-[calc(28.57%+14.43px)] top-0 w-[1066px]" />
       <Frame13 />
-      <Frame21 />
       <Frame20 />
-      <div className="absolute font-['Pretendard:Regular',sans-serif] leading-[25px] left-[calc(4.76%+18.57px)] not-italic text-[13px] text-white top-[calc(19.05%+16.29px)] tracking-[-0.26px] uppercase w-[342px]">
-        <p className="mb-0">{`네거티브 프릭은 'Unexpected Beauty(예상치 못한 아름다움)'를 슬로건으로 하는 100% AI 제작 가상의 뷰티 브랜드입니다.`}</p>
-        <p className="mb-0">기존 뷰티 시장이 강요하는 완벽,결점을 커버하는 키워드에 반기를 들고, 디지털 오류와 의도된 불완전함에서 오는 새로운 미학을 탐구합니다.</p>
-        <p className="mb-0">&nbsp;</p>
-        <p>제품 기획부터 네이밍, 패키지 디자인, 가상 모델, 비주얼 캠페인까지. 물리적 제약이 없는 가상 공간에서 AI 기술과 디렉터의 기획력만으로 탄생시킨 차세대 뷰티 브랜드입니다.</p>
-      </div>
       <Frame11 />
       <div className="absolute flex flex-col font-['Pretendard:Bold',sans-serif] h-[30px] justify-center leading-[0] left-[calc(85.71%+254.29px)] not-italic text-[23px] text-right text-white top-[35px] tracking-[-0.46px] translate-x-[-100%] translate-y-[-50%] uppercase w-[251px]">
         <p className="leading-[35px]">COntact</p>
